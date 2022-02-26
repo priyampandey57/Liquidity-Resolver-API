6fbce8b59ed2692bc0c7789f9a66feab6a134617
priyampandey57 <97950315+priyampandey57@users.noreply.github.com>
2022-02-26 23:55:24 +0530

    Update router.js

--- router.js
+++ router.js
@@ -1,29 +1,21 @@
-const Web3 = require('web3');
+var Web3 = require('web3');
 const express = require('express');
 const router = express.Router();
 const config = require('./config.json');
+const ethers = require("ethers");
 
-//const router = new Router();
-
-const web3 = new Web3(process.env.Alchemy_url);
-
-const ContractIns =  new web3.eth.Contract(config.ContractABI, config.ContractAddr);
-    
+var web3 = new Web3(new Web3.providers.HttpProvider('https://polygon-mainnet.g.alchemy.com/v2/YWi_k9aPJuQd0lqPkKnmY_KWsPRUvPfb'));
+const ContractIns = new web3.eth.Contract(config.ContractABI, config.ContractAddr);
 router.get('/liquidityResolver/', async (req, res) => {
-    let protocol = req.query.protocol;
-    let token = req.query.token;
-    
-    let result = await ContractIns
-    .methods
-    .getInfo([protocol], [token])
-    .call()
+    let protocol = req.query.protocol.split(',');
+    let token = req.query.token.split(',');
+    let result = await ContractIns.methods.getInfo(protocol, token).call()
     return res.send(result);
+})
 
-    })
-
-    router.get('/', async (req, res) => {
-let result = "server running";
-return res.send(result);
-    })
+router.get('/', async (req, res) => {
+    let result = "server running";
+    return res.send(result);
+})
 
 module.exports = router;
